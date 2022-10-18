9ec9a16af9c337d9ea5447ba8428b6149368067a
Farhad Towfighian <fartow@gmail.com>
2022-10-18 14:33:04 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class App {
+    public static void main(String args[]) {
+        String[] cityNames = {"Paris", "London", "New York", "Stockholm"};
+        int size = cityNames.length;
+
+        for(int i = 0; i<size-1; i++) {
+            for (int j = i+1; j< cityNames.length; j++) {
+                if(cityNames[i].compareTo(cityNames[j])>0) {
+                    String temp = cityNames[i];
+                    cityNames[i] = cityNames[j];
+                    cityNames[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(cityNames));
+    }
+}
